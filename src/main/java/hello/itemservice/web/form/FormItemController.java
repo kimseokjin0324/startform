b2ebbe9f245c0a4dfeb32474d710f20e2cb9ac35
kimseokjin <kimseokjin0324@gmail.com>
2021-09-21 15:21:44 +0900

    타임리프(스프링 통합과 폼)-라디오 버튼

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -2,6 +2,7 @@ package hello.itemservice.web.form;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -27,6 +28,10 @@ public class FormItemController {
         regions.put("BUSAN","부산");
         regions.put("JEJU","제주");
         return regions;
+    }
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes(){
+        return ItemType.values();
     }
 
     @GetMapping
@@ -54,6 +59,8 @@ public class FormItemController {
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
         log.info("item.open={}",item.getOpen());
         log.info("item.regions={}",item.getRegions());
+        log.info("item.itemType={}",item.getItemType());
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
